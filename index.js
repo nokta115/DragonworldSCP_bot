df309b6e5cc776236ac66c8a98d89667cfc77e52
Glitch (hello-express) <none>
2020-10-08 19:40:54 +0000

    ⛵️🍉 Checkpoint ./server.js:23561509/1 ./botconfig.json:23561509/95 ./index.js:23561509/855

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js'); 
+const bot = new Discord.Client();
+//подключаем файл конфигурации
+let config = require('./botconfig.json'); 
+//"достаём" токен и префикс
+let token = config.token; 
+let prefix = config.prefix;
+//создаём ссылку-приглашение для бота
+bot.on('ready', () => { 
+    console.log(`Запустился бот ${bot.user.username}`);
+    bot.generateInvite(["ADMINISTRATOR"]).then(link => { 
+        console.log(link);
+    });
+});
+//команда, и то, что она должна выполнить
+bot.on('message', msg => {
+    if (msg.content === prefix + 'привет') {
+        msg.reply('ПРИВЕТ, ЛОЛ!');
+    }
+});
+bot.on('message', msg => {
+    if (msg.content === prefix + 'АУУУУУ') {
+        msg.channel.send('@everyone ВЫ ГДЕЕЕЕ');
+    }
+});
+bot.on('message', msg => {
+    if (msg.content === 'ммм') {
+        msg.reply('НЕ МЫ ЧИ');
+    }
+});
+bot.login(token);
